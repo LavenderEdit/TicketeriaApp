08ebb848fa1d4b39734136e6c5c6e970cf4076df
Lavender <gercermagden@gmail.com>
2025-03-27 22:17:44 -0500

    chore(scripts): Reestructuración

--- js/script.js
+++
@@ -1,7 +0,0 @@
-import { setupModalCloseButtons } from './script-modales.js';
-import { togglePassword } from './script-general-functions.js'
-
-document.addEventListener('DOMContentLoaded', () => {
-    setupModalCloseButtons();
-    togglePassword();
-});
