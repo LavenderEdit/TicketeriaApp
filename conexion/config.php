9db1a818032ae5986641a86da64bf2e678c5fab6
LionelPastranaMK500 <andriypastrana1@gmail.com>
2025-04-11 20:15:01 -0500

    refactor(database): reestructurar conexión a la base de datos y mover archivos de configuración

--- conexion/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'DB_HOST' => 'bd-g3.mysql.database.azure.com',
-    'DB_NAME' => 'TicketingSystemDB',
-    'DB_USER' => 'g3Admin',
-    'DB_PASS' => 'Serfelizen29'
-];
---
+++ database/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'db_host' => 'bd-g3.mysql.database.azure.com',
+    'db_name' => 'TicketingSystemDB',
+    'db_USER' => 'g3Admin',
+    'db_pass' => 'Serfelizen29',
+];
